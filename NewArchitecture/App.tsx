ed3ff6906d452fdd6007123814bf225d9ae4c06a
shagun-firstlight <73980122+shagun-firstlight@users.noreply.github.com>
2023-06-01 13:03:28 +0530

    add slider

--- NewArchitecture/App.tsx
+++ NewArchitecture/App.tsx
@@ -14,16 +14,35 @@ StatusBar,
 Text,
 Button,
 } from 'react-native';
+import Slider from '@react-native-community/slider';
 import {calculator, ColoredView} from 'example-library/src/index'
 const App: () => Node = () => {
 const [currentResult, setResult] = useState<number | null>(null);
 const [currentResult1, setResult1] = useState<number | null>(null);
 const [currentResult2, setResult2] = useState<number | null>(null);
-
+const [sliderValue, setSliderValue] = useState(0);
 return (
     <SafeAreaView>
     <StatusBar barStyle={'dark-content'}/>
+
+    <Slider
+  style={{width: "100%", height: 40, marginTop:100}}
+  minimumValue={0}
+  maximumValue={100}
+  minimumTrackTintColor="#307ecc"
+  maximumTrackTintColor="#000000"
+  step={1}
+  value={sliderValue}
+onValueChange={
+(sliderValue) => {setSliderValue(sliderValue)
+console.log("shagun", sliderValue)
+}
+}
+/>
+
     <ColoredView color="#FF0099" style={{marginLeft:10, marginTop:20, width:100, height:100}}/>
+
+
 
     <Text style={{marginLeft:20, marginTop:20}}>3+7={currentResult ?? "??"}</Text>
     <Button title="Compute" onPress={async () => {
